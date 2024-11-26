0f90bf04a7d69ca4f12a56fe486fb69ca29572a2
Peter Weißbier <140187896+peterweissbier@users.noreply.github.com>
2024-11-26 12:30:20 +0100

    Update rutracker.py

--- rutracker.py
+++ rutracker.py
@@ -6,8 +6,8 @@
 class Config(object):
     # Replace `YOUR_USERNAME_HERE` and `YOUR_PASSWORD_HERE` with your RuTracker username and password
     # Do not remove the `u` marker nor the '' quote characters
-    username = u'YOUR_USERNAME_HERE'
-    password = u'YOUR_PASSWORD_HERE'
+    username = u'seether5'
+    password = u't7fxUFRBBZF1G1'
 
     # If you want to use magnet links instead of torrent files for downloading, uncomment `download_type`
     # download_type = 'MAGNET_LINK'
